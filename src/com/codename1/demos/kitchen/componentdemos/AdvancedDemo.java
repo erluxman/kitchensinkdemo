5e1653ad97601beff27a43eb73b3026bd25f6acc
Laxman Bhattarai <erluxman@gmail.com>
2020-02-01 14:36:34 +0545

    performance tuning

--- src/com/codename1/demos/kitchen/componentdemos/AdvancedDemo.java
+++ src/com/codename1/demos/kitchen/componentdemos/AdvancedDemo.java
@@ -48,58 +48,84 @@ class AdvancedDemo extends DemoComponent {
         });
 
 
-        SignatureComponent signatureComponent = new SignatureComponent();
         Button showSignature = new Button("Sign the Document");
-        Form viewer = new Form("Browser Demo", new BorderLayout());
-        viewer.add(CENTER, BorderLayout.center(signatureComponent));
-        viewer.getToolbar().setBackCommand("", ee -> showSignature.getComponentForm().showBack());
         showSignature.addActionListener(evt -> {
+            SignatureComponent signatureComponent = new SignatureComponent();
+            Form viewer = new Form("Browser Demo", new BorderLayout());
+            viewer.add(CENTER, BorderLayout.center(signatureComponent));
+            viewer.getToolbar().setBackCommand("", ee -> showSignature.getComponentForm().showBack());
             viewer.show();
         });
 
-        FileTree fileTree = new FileTree();
-        fileTree.setScrollableY(false);
-        ImageViewer imageViewer = new ImageViewer();
-        ListModel<Image> images = new DefaultListModel<Image>(
-                resources.getImage("background.jpg"),
-                resources.getImage("layout.png"),
-                resources.getImage("themes.png"),
-                resources.getImage("dog.jpg")
-        );
-
-        Button previousButton = new Button("Previous");
-        Button nextButton = new Button("Next");
-        Container buttonLine = new Container(BoxLayout.xCenter())
-                .add(previousButton)
-                .add(nextButton);
-        Container imageViewerContainer = new Container(BoxLayout.y())
-                .add(imageViewer).add(buttonLine);
-        previousButton.addActionListener(event -> {
-            int currentIndex = images.getSelectedIndex();
-            int totalSize = images.getSize();
-            currentIndex -= 1;
-            if (currentIndex < 0) {
-                currentIndex = totalSize - 1;
-            }
-            images.setSelectedIndex(currentIndex);
+        Button showFiles = new Button("Show Files");
+        showFiles.addActionListener(evt -> {
+            Form viewer = new Form("Browser Demo", new BorderLayout());
+            FileTree fileTree = new FileTree();
+            fileTree.setScrollableY(false);
+            viewer.add(CENTER, BorderLayout.center(fileTree));
+            viewer.getToolbar().setBackCommand("", ee -> showFiles.getComponentForm().showBack());
+            viewer.show();
         });
 
-        nextButton.addActionListener(event -> {
-            int currentIndex = images.getSelectedIndex();
-            currentIndex += 1;
-            if (currentIndex >= images.getSize()) {
-                currentIndex = 0;
-            }
-            images.setSelectedIndex(currentIndex);
-        });
-        imageViewer.setImageList(images);
 
+        Button showImageViewer = new Button("Show Image Viewer");
+        showImageViewer.addActionListener(evt -> {
+            ListModel<Image> images = new DefaultListModel<Image>(
+                    resources.getImage("background.jpg"),
+                    resources.getImage("layout.png"),
+                    resources.getImage("themes.png"),
+                    resources.getImage("dog.jpg")
+            );
+
+            Button previousButton = new Button("Previous");
+            Button nextButton = new Button("Next");
+            Container buttonLine = new Container(BoxLayout.xCenter())
+                    .add(previousButton)
+                    .add(nextButton);
+            previousButton.addActionListener(event -> {
+                int currentIndex = images.getSelectedIndex();
+                int totalSize = images.getSize();
+                currentIndex -= 1;
+                if (currentIndex < 0) {
+                    currentIndex = totalSize - 1;
+                }
+                images.setSelectedIndex(currentIndex);
+            });
+
+            nextButton.addActionListener(event -> {
+                int currentIndex = images.getSelectedIndex();
+                currentIndex += 1;
+                if (currentIndex >= images.getSize()) {
+                    currentIndex = 0;
+                }
+                images.setSelectedIndex(currentIndex);
+            });
+
+            Form viewer = new Form("Image Viewer", new BorderLayout());
+            ImageViewer imageViewer = new ImageViewer();
+            Container imageViewerContainer = new Container(BoxLayout.y())
+                    .add(imageViewer).add(buttonLine);
+            imageViewer.setImageList(images);
+            viewer.add(CENTER, BorderLayout.center(imageViewerContainer));
+            viewer.getToolbar().setBackCommand("", ee -> showImageViewer.getComponentForm().showBack());
+            viewer.show();
+        });
+
+        Button showCalendar = new Button("Show Calendar");
+        showCalendar.addActionListener(evt -> {
+            Form viewer = new Form("Calendar Viewer", new BorderLayout());
+            Container imageViewerContainer = new Container(BoxLayout.y())
+                    .add(new Calendar());
+            viewer.add(CENTER, BorderLayout.center(imageViewerContainer));
+            viewer.getToolbar().setBackCommand("", ee -> showCalendar.getComponentForm().showBack());
+            viewer.show();
+
+        });
         demo.add("Browser Component", showBrowserComponent);
         demo.add("SignatureComponent", showSignature);
-        demo.add("Calendar", new Calendar());
-        demo.add("FileTree", fileTree);
-        demo.add("Image Viewer", imageViewerContainer);
+        demo.add("Calendar", showCalendar);
+        demo.add("FileTree", showFiles);
+        demo.add("Image Viewer", showImageViewer);
         return demo.generate();
     }
-
 }
