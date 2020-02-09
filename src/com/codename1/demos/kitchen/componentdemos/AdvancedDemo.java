86a0e4c431361e38346ca167280fec7b2af9670a
Laxman Bhattarai <erluxman@gmail.com>
2020-02-09 15:47:44 +0545

    added advanced component info and java docs

--- src/com/codename1/demos/kitchen/componentdemos/AdvancedDemo.java
+++ src/com/codename1/demos/kitchen/componentdemos/AdvancedDemo.java
@@ -121,11 +121,25 @@ class AdvancedDemo extends DemoComponent {
             viewer.show();
 
         });
-        demo.add("Browser Component", showBrowserComponent);
-        demo.add("SignatureComponent", showSignature);
-        demo.add("Calendar", showCalendar);
-        demo.add("FileTree", showFiles);
-        demo.add("Image Viewer", showImageViewer);
+        demo.add("Browser Component", showBrowserComponent,browserInfo,BrowserComponent.class);
+        demo.add("SignatureComponent", showSignature,signatureInfo,SignatureComponent.class);
+        demo.add("Calendar", showCalendar,calendarInfo,Calendar.class);
+        demo.add("FileTree", showFiles,fileTreeInfo,FileTree.class);
+        demo.add("Image Viewer", showImageViewer,imageViewerInfo,ImageViewer.class);
         return demo.generate();
     }
+
+    String browserInfo ="The browser component is an interface to an embeddable native platform browser on platforms that support embedding the native browser in place, if you need wide compatibility and flexibility you should check out the HTMLComponent which provides a lightweight 100% cross platform web component.\n" +
+            "This component will only work on platforms that support embedding a native browser which exclude earlier versions of Blackberry devices and J2ME devices.\n" +
+            "Its recommended that you place this component in a fixed position (none scrollable) on the screen without other focusable components to prevent confusion between focus authority and allow the component to scroll itself rather than CodenameOne making that decision for it.\n" +
+            "\n" +
+            "On Android this component might show a native progress indicator dialog. You can disable that functionality using the call.";
+    String signatureInfo ="A component to allow a user to enter their signature. This is just a button that, when pressed, will pop up a dialog where the user can draw their signature with their finger. The user is given the option to save/reset/cancel the signature. On save, the signatureImage property will be set with a full-size image of the signature, and the icon on the button will show a thumbnail of the image.";
+    String calendarInfo ="Date widget for selecting a date/time value." +
+            "To localize strings for month names use the values Calendar.Month using the 3 first characters of the month name in the resource localization e.g. Calendar.Jan, Calendar.Feb etc..." +
+            "To localize strings for day names use the values Calendar.Day in the resource localization e.g. \"Calendar.Sunday\", \"Calendar.Monday\" etc..." +
+            "\n" +
+            "Note that we recommend using the Picker class which is superior when running on the device for most use cases.";
+    String fileTreeInfo ="Simple class showing off the filesystem as a tree component";
+    String imageViewerInfo ="ImageViewer allows zooming/panning an image and potentially flicking between multiple images within a list of images.";
 }
